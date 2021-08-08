7c1990048dde915308b7f6730267ede3a107f64d
Masayoshi Mizutani <mizutani@cookpad.com>
2021-08-08 20:00:38 +0900

    Access to github

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/m-mizutani/cs-alert-notify
 
 go 1.16
+
+require (
+	github.com/google/go-github/v37 v37.0.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210805134026-6f1e6394065a // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/google/go-github/v37/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
 	fmt.Println("Hello, Hello, Hello")
 	for _, v := range os.Environ() {
 		fmt.Println(v)
+	}
+
+	ctx := context.Background()
+	tc := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	))
+	client := github.NewClient(tc)
+
+	owner := os.Getenv("GITHUB_REPOSITORY_OWNER")
+	repo := os.Getenv("GITHUB_REPOSITORY")
+	alerts, resp, err := client.CodeScanning.ListAlertsForRepo(ctx, owner, repo, &github.AlertListOptions{})
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("failure: %d\n", resp.StatusCode)
+	}
+
+	for _, alert := range alerts {
+		fmt.Printf("alert: %v\n", alert)
 	}
 }
